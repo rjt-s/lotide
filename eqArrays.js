47c5f86d33f1ba4d74a4a835063c85d8af87fd1f
Rajat Sharma <srma.rjt@gmail.com>
2022-11-15 22:09:06 -0500

    Array eq func added

---
+++ eqArrays.js
@@ -0,0 +1,39 @@
+// Comparing Arrays
+
+// copying assertEqual function
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+// implementing eqArray function
+const eqArrays = function(arr1,arr2) {
+  if(arr1.length !== arr2.length) {
+    return false;
+  }
+  for(let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true; 
+};
+
+// Test Cases
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+// console.log(eqArrays(['abc', 'xyz', 3, 4], ['abc', 'xyz', 3, 4]))
+
+// Testing with Assert Equal
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), true);
+
+
