5a7421ec91e8ff0c6ec1562e5ecb96f660e40e6f
Rajat Sharma <srma.rjt@gmail.com>
2022-11-15 22:29:17 -0500

    Assert Equal Arrays Updated

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+// assert Equal Arrays func
+
+// copying eqArrays func
+const eqArrays = function(arr1,arr2) {
+  if(arr1.length !== arr2.length) {
+    return false;
+  }
+  for(let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true; 
+};
+
+// Implementing assertArraysEqual func
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+// Test Cases
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], ['1', 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual(['a', 'b', 'c'], ['a', 'b', 'c']);
+assertArraysEqual(['a', 'b', 'c'], ['a', 'b', 'C']);
