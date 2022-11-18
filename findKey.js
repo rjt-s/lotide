5fa8e5b62a3c3306f2243bf361b18819c9e53266
Rajat Sharma <srma.rjt@gmail.com>
2022-11-17 23:08:30 -0500

    findKey function added

---
+++ findKey.js
@@ -0,0 +1,40 @@
+// findKey function implementation
+// copying assertEqual function
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+// findKey function
+const findKey = function(obj, callback) {
+  for(let key in obj) {
+    if(callback(obj[key])){
+      return key;
+    }
+  }
+}
+
+// Testing the findkey func
+let testObj1 = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}
+let testObj2 = {
+  'USA' : 'Washington DC',
+  'Canada' : 'Ottawa',
+  'U.K.' : 'London',
+  'France': 'Paris',
+  'Germany': 'Berlin'
+}
+// console.log(findKey(testObj1, x => x.stars === 2));
+// console.log(findKey(testObj2, x => x === 'Ottawa'));
+assertEqual(findKey(testObj1,x => x.stars === 2), 'noma');
+assertEqual(findKey(testObj2, x => x === 'Ottawa'), 'Canada');
+
