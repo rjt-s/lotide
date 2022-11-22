c0a5f6be18f73f4357c7ed3f0eaf0209754c020c
Rajat Sharma <srma.rjt@gmail.com>
2022-11-22 15:09:18 -0500

    updated assertObjectsEqual

---
+++ assertObjectsEqual.js
@@ -0,0 +1,18 @@
+// Implementing assertObjectEqual function
+// importing eqObjects
+const eqObjects = require('./eqObjects');
+
+// FUNCTION IMPLEMENTATION
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  // Implement me!
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+// tests for function
+assertObjectsEqual({a:1,b:2,c:[1,2,3]},{c:[1,2,3],b:2,a:1});
+assertObjectsEqual({a:1},{a:1,b:2});
